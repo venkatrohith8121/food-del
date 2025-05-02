55e7e9745b9f0856e557a9734df8eab1c3b837ae
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2025-05-02 18:11:59 +0530

    home components

---
+++ Frontend/src/Components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ExploreMenu.css'
+import {menu_list} from '../../assets/assets'
+
+const ExploreMenu = () => {
+  return (
+    <div>ExploreMenu</div>
+  )
+}
+
+export default ExploreMenu
--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 const Navbar = () => {
-    const [menu, setMenu] = useState("home");
+    const [menu, setMenu] = useState("Home");
     
     return (
         <div className="navbar">
             <img src={assets.logo} alt="" className='logo' />
             <ul className="navbar-menu">
-                < li onClick={()=>{setMenu("home")}} className={menu==="home"?"active":""}>home</ li>
+                < li onClick={()=>{setMenu("Home")}} className={menu==="Home"?"active":""}>Home</ li>
                 < li onClick={()=>{setMenu("menu")}} className={menu==="menu"?"active":""}>menu</ li>
                 < li onClick={()=>{setMenu("mobile-app")}} className={menu==="mobile-app"?"active":""}>mobile-app</ li>
                 < li onClick={()=>{setMenu("contact-us")}} className={menu==="contact-us"?"active":""}>contact-us</ li>
