108b8a115e1b27a8544eed246af40ec53db8044c
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2025-05-04 13:01:17 +0530

    frontend done almost

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -11,19 +11,20 @@ const StoreContextProvider = (props) => {
             setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
     }
-    const removeFromCart=(itemId)=>{
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
-    } 
-    useEffect(()=>{
-        console.log(cartItems)
-    },[cartItems])
-    const contextValue = {
-        food_list,
-        cartItems,
-        setCartItems,
-        addToCart,
-        removeFromCart
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
+    const contextValue = { food_list, cartItems, setCartItems, addToCart, removeFromCart, getTotalCartAmount }
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
